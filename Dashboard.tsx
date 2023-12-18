c5282da8d3223a8d4bf0de71496456b0b5b81b9a
aaruph <aaruph12345@gmail.com>
2023-12-18 10:29:13 +0545

    dashboard frontend completed

---
+++ Dashboard.tsx
@@ -0,0 +1,128 @@
+className={"product-card"}>
+                    <img src={"images/7.jpg"} alt="Product 5"/>
+                    <div className={"product-info"}>
+                        <div className={"product-title"}>New Balance Men's Cotton Performance V-Neck Undershirt (3 Pack)</div>
+                        <div className={"product-price"}>$19.99</div>
+                    </div>
+                    <ul>
+                        <li><i className={"fa fa-star checked"}></i></li>
+                        <li><i className={"fa fa-star checked"}></i></li>
+                        <li><i className={"fa fa-star checked"}></i></li>
+                        <li><i className={"fa fa-star checked"}></i></li>
+                        <li><i className={"fa fa-star"}></i></li>
+
+                    </ul>
+                    <button>Buy Now</button>
+                </div>
+                <div className={"product-card"}>
+                    <img src={"images/8.jpg"} alt="Product 5"/>
+                    <div className={"product-info"}>
+                        <div className={"product-title"}>Bbonlinedress Sparkly Sequins Glitter Wedding Guest Cocktail Prom Party Mini Formal Dress Elegant Flowy Bridesmaid Dress</div>
+                        <div className={"product-price"}>$23.99</div>
+                    </div>
+                    <ul>
+                        <li><i className={"fa fa-star checked"}></i></li>
+                        <li><i className={"fa fa-star checked"}></i></li>
+                        <li><i className={"fa fa-star checked"}></i></li>
+                        <li><i className={"fa fa-star checked"}></i></li>
+                        <li><i className={"fa fa-star"}></i></li>
+
+                    </ul>
+                    <button>Buy Now</button>
+                </div>
+                <div className={"product-card"}>
+                    <img src={"images/9.jpg"} alt="Product 5"/>
+                    <div className={"product-info"}>
+                        <div className={"product-title"}>JMIERR Mens Quilted Shirt Jackets Casual Long Sleeve Snap Button Down Shirts with Pockets</div>
+                        <div className={"product-price"}>$39.99</div>
+                    </div>
+                    <ul>
+                        <li><i className={"fa fa-star checked"}></i></li>
+                        <li><i className={"fa fa-star checked"}></i></li>
+                        <li><i className={"fa fa-star checked"}></i></li>
+                        <li><i className={"fa fa-star checked"}></i></li>
+                        <li><i className={"fa fa-star"}></i></li>
+
+                    </ul>
+                    <button>Buy Now</button>
+                </div>
+                <div className={"product-card"}>
+                    <img src={"images/jogger.jpg"} alt="Product 5"/>
+                    <div className={"product-info"}>
+                        <div className={"product-title"}>Under Armour Mens Hustle Fleece Jogger Pant</div>
+                        <div className={"product-price"}>$26.99</div>
+                    </div>
+                    <ul>
+                        <li><i className={"fa fa-star checked"}></i></li>
+                        <li><i className={"fa fa-star checked"}></i></li>
+                        <li><i className={"fa fa-star checked"}></i></li>
+                        <li><i className={"fa fa-star checked"}></i></li>
+                        <li><i className={"fa fa-star"}></i></li>
+
+                    </ul>
+                    <button>Buy Now</button>
+                </div>
+
+
+
+
+
+
+
+
+
+
+            </div>
+
+
+
+
+            <div className={"footer"}>
+                <div className={"get-help"}>
+                    <h1>GET HELP</h1>
+                    <button>Customer Care</button>
+                    <button>Payment Options</button>
+                    <button>Return and Refund Policy</button>
+                    <button>Privacy Policy</button>
+                    <button>Terms and Conditions</button>
+                    <span>@2023 Lugahub Pvt. Ltd. All Rights Reserved</span>
+
+                </div>
+                <div className={"about-us"}>
+                    <h1>LUGAHUB</h1>
+                    <button>About Us</button>
+                    <button>Contact Us</button>
+                    <button>Careers</button>
+                    <button>Guide</button>
+
+
+
+                </div>
+                <div className={"logos"}>
+                    <span>Connect with us:</span>
+                    <img
+                        width={30}
+                        src={"images/fb.png"}
+                        alt="fb"
+                        />
+                    <img
+                        width={31}
+                        src={"images/insta.png"}
+                        alt="fb"
+                    />
+                    <img
+                        width={35}
+                        src={"images/twitter.png"}
+                        alt="fb"
+                    />
+
+
+                </div>
+
+            </div>
+        </div>
+
+    );
+};
+
+export default Dashboard;
